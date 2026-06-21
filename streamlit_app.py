1ee8bb0707407a94d070552f2a9cb95983c45ac2
bhawnadhapola94-sys <bhawnadhapola94@gmail.com>
2026-06-21 13:23:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,45 +23,42 @@ st.header("Overview")
 col1, col2 = st.columns(2)
 
 with col1:
-   provider_count = provider['Type'].value_counts().sort_index()
+    st.subheader("Provider Types Distribution")
+    fig, ax = plt.subplots(figsize=(5,3))
+    provider['Type'].value_counts().plot(
+        kind='line',
+        marker='o',
+        ax=ax
+    )
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(5,3))
-provider_count.plot(
-    kind='line',
-    marker='o',
-    linewidth=2,
-    ax=ax
-)
-
-ax.set_xlabel("Provider Type")
-ax.set_ylabel("Count")
-ax.set_title("Provider Type Distribution")
-plt.xticks(rotation=45)
-
-st.pyplot(fig)
 with col2:
     st.subheader("Receiver Types Distribution")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5,3))
     receivers['Type'].value_counts().plot(kind='bar', ax=ax)
+    plt.xticks(rotation=45)
     st.pyplot(fig)
 
-col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
 
-with col1:
-    location_count = (food.groupby('Location')['Food_Name'].count().sort_values(ascending=False).head(10))
+with col3:
+    st.subheader("Top 10 Locations")
+    top_locations = (
+        food.groupby('Location')['Food_Name']
+        .count()
+        .sort_values(ascending=False)
+        .head(10)
+    )
 
-location_count.plot(kind='barh', ax=ax)
-ax.set_xlabel("Food Count")
-ax.set_ylabel("Location")
-ax.set_title("Top 10 Locations by Food Count")
+    fig, ax = plt.subplots(figsize=(5,3))
+    top_locations.plot(kind='bar', ax=ax)
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
 
-with col2:
+with col4:
     st.subheader("Meal Types")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(5,3))
     food['Meal_Type'].value_counts().plot(kind='bar', ax=ax)
+    plt.xticks(rotation=45)
     st.pyplot(fig)
-
-st.markdown("""
-This dashboard provides insights into food donations,
-providers, receivers, and claims to help reduce food wastage.
-""")
